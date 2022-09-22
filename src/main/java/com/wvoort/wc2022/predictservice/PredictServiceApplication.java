51379713f34c803ab7fd4bf086bcb7d24dfdda73
Wim van der Voort <wvoort.66@gmail.com>
2022-09-22 17:01:04 +0100

    first commit

---
+++ src/main/java/com/wvoort/wc2022/predictservice/PredictServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wvoort.wc2022.predictservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wvoort/wc2022/predictservice/PredictServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wvoort.wc2022.predictservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
