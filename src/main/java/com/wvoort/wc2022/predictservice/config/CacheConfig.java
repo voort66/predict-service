8a6e942c748e5e84db316206a4e652265428eb7f
Wim van der Voort <wvoort.66@gmail.com>
2022-11-06 10:52:08 +0000

    * Cache eviction once a day

--- src/main/java/com/wvoort/wc2022/predictservice/config/CacheConfig.java
+++ src/main/java/com/wvoort/wc2022/predictservice/config/CacheConfig.java
@@ -1,16 +1,24 @@
 package com.wvoort.wc2022.predictservice.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import javax.management.timer.Timer;
+import java.time.Instant;
 import java.util.Arrays;
 
+@Slf4j
 @Configuration
 @EnableCaching
+@EnableScheduling
 public class CacheConfig {
 
     private static final String MATCHES = "matches";
@@ -20,6 +28,12 @@ public class CacheConfig {
         SimpleCacheManager cacheManager = new SimpleCacheManager();
         cacheManager.setCaches(Arrays.asList(new ConcurrentMapCache(MATCHES)));
         return cacheManager;
+    }
+
+    @CacheEvict(allEntries = true, value = {MATCHES})
+    @Scheduled(fixedDelay = Timer.ONE_DAY)
+    public void reportCacheEvict() {
+        log.info("Fixtures cache flushed at {}", Instant.now());
     }
 
 }
